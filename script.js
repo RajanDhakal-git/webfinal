d3187a52502e24893bc34fd1d8c3eaed437e9655
RajanDhakal-git <90204005+RajanDhakal-git@users.noreply.github.com>
2025-01-01 14:24:33 +0545

    crousal_complete

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let Popup = document.querySelector('.popup')
-let openpopup = ()=> Popup.classList.add('open-popup')
-let closepopup = ()=> Popup.classList.remove('open-popup')
+const lists = document.querySelector(".lists");
+const prevButton = document.querySelectorAll("#btns button")[0];
+const nextButton = document.querySelectorAll("#btns button")[1];
+
+const cardWidth = document.querySelector(".card").offsetWidth + 20; // Includes margin
+
+// Move the carousel left
+prevButton.addEventListener("click", () => {
+  lists.scrollBy({
+    left: -cardWidth,
+    behavior: "smooth",
+  });
+});
+
+// Move the carousel right
+nextButton.addEventListener("click", () => {
+  lists.scrollBy({
+    left: cardWidth,
+    behavior: "smooth",
+  });
+});

e6e24d3a189443fd92fcd5dd5d82f2facd88073e
RajanDhakal-git <90204005+RajanDhakal-git@users.noreply.github.com>
2025-01-04 12:11:26 +0545

    gallery complete

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let photos = [1,2,3,4,5,6]
-let gallery_container = document.querySelector('.gallery_container')
-photos.forEach((item)=>{
-    let div = document.createElement('div')
-    div.classList.add('card')
-    div.innerHTML = `<img src='public/${item}.jpg /'>`
-    gallery_container.appendChild('div')
-})
+let photos = [1, 2, 3, 4, 5, 6, 7, 8,9];
+let gallery_container = document.querySelector('.gallery_container');
+
+// Dynamically create cards with images
+photos.forEach((item, i) => {
+    let div = document.createElement('div');
+    div.classList.add('card', `img-${i}`);
+
+    gallery_container.appendChild(div);
+    let box = document.createElement('div')
+    box.classList.add('box')
+    box.innerHTML = `<img src="public/${item}.jpg" alt="Photo ${item}">`;
+    div.appendChild(box)
+});

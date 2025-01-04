c48fbab061265f9e125df41084eeaa81cac59818
RajanDhakal-git <90204005+RajanDhakal-git@users.noreply.github.com>
2025-01-04 11:40:11 +0545

    start

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let Popup = document.querySelector('.popup')
-let openpopup = ()=> Popup.classList.add('open-popup')
-let closepopup = ()=> Popup.classList.remove('open-popup')
+let photos = [1,2,3,4,5,6]
+let gallery_container = document.querySelector('.gallery_container')
+photos.forEach((item)=>{
+    let div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `<img src='public/${item}.jpg /'>`
+    gallery_container.appendChild('div')
+})
